52ad0349bf49725615b29fd945ad2e866c57c3f2
persit <402763782@qq.com>
2020-12-02 17:09:53 +0800

    asdd

--- citizen.c
+++ citizen.c
@@ -6,7 +6,7 @@ int main()
     char str[5];
     char str2[]={"123456789909090"};
     strcpy(str,str2);
-    printf{“hello氨基酸地方}
+    printf{“hello氨基酸地子啊啊士大夫阿斯蒂芬方}
     printf("%s\n",str);
-    return 0;
+    return 0;dasdfasdf
 }
